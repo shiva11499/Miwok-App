5c89bf34705401bfb1b7b1303c34f768bfdac40c
shiva11499 <gshivansh37@gmail.com>
2019-10-24 00:55:56 +0530

    Activity lifecycle updated

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -21,6 +21,13 @@ public class NumbersActivity extends AppCompatActivity {
     };
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
